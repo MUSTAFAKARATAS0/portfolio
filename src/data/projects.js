25d0ac092ad5b0676d3d1d291722fbc3aefea430
Mustafa KARATAŞ <91211621+MUSTAFAKARATAS0@users.noreply.github.com>
2025-08-06 10:36:31 +0300

    add project

--- src/data/projects.js
+++ src/data/projects.js
@@ -5,14 +5,27 @@ import ship from "../assets/images/gemi.jpeg";
 import dog from "../assets/images/dog.jpeg";
 import scroll from "../assets/images/scroll.jpg";
 import wow from "../assets/images/wow.webp";
+import pms from "../assets/images/image.png";
 
 export const projects = [
+  {
+    title: "Project Management System",
+    image:pms,
+    github: "https://github.com/MUSTAFAKARATAS0/Project-Management-System",
+    description:" A project management system that allows users to create, manage, and track projects.",
+  },
   {
     title: "World Of Wonders",
     image: wow,
     github: "https://github.com/MUSTAFAKARATAS0/WorldOfWonders",
     description:
       "The website showing the best places to visit in the world",
+  },
+   {
+    title: "Film Archive",
+    image: farsiv,
+    github: "https://github.com/MUSTAFAKARATAS0/F-Arsiv",
+    description:"A mobile application where people can get information about movies.",
   },
   {
     title: "Maritime Trade Routes ",
@@ -27,17 +40,13 @@ export const projects = [
     image:rdt,
     github: "https://github.com/MUSTAFAKARATAS0/Diabetic-Retinopathy",
   },
-  {
-    title: "Film Archive",
-    image: farsiv,
-    github: "https://github.com/MUSTAFAKARATAS0/F-Arsiv",
-    description:"A mobile application where people can get information about movies.",
-  },
+ 
   {
     title: "Dog Scope",
     image:dog,
     github: "https://github.com/MUSTAFAKARATAS0/Dog-Scope",
     description:"A project that shows which breed the dog photos uploaded to the system belong to.",
   },
+  
   
 ];
